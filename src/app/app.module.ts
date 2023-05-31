31d69a49b5a96d5791c0c65f9790691072c05a33
bmonroyg <bmonroyg@emeal.nttdata.com>
2023-05-30 20:00:41 -0500

    section flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { ProductsModule } from './products/products.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basic-page.component.html',
+  styleUrls: ['./basic-page.component.css']
+})
+export class BasicPageComponent {
+
+  public nameLower: string = 'fernando herrera';
+  public nameUpper: string = 'FERNANDO HERRERA';
+  public nameComplete: string = 'FeRnAnDo HeRrErA';
+
+}
